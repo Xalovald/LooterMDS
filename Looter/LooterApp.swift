c132b9b85c781e9be6ef01b8d5f6f2fe0f17b5f6
Guilian <guilian.bahno@laposte.net>
2024-10-08 16:22:09 +0200

    first commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by eduadmin on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
